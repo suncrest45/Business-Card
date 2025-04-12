ec9c2154b94679acf42ed64038ae1635dca2bdd8
suncrest45 <suncrest45@users.noreply.github.com>
2025-04-11 20:31:25 -0700

    Displaying all cards in rows of 5 & implemented scrolling

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ let height = window.innerHeight || document.documentElement.clientHeight || docu
 let cards = {};
 let names = new Object();
 let count = 0;
+let row = 0;
+let column = 0;
+let xOffset = 400;
 
 
 console.log(width);
@@ -24,14 +27,19 @@ async function readCSV(csvFilePath) {
          names[String(d.Name)] = Number(count);
          let card = svg.append("image")
             .attr("xlink:href", "../Data/" + d.Name + "_front.jpg")
-            .attr("x", 0)            // x-coordinate
-            .attr("y", 0)            // y-coordinate
+            .attr("x", 500 * column)            // x-coordinate
+            .attr("y", 500 * row)            // y-coordinate
             .attr("width", 500)      // rectangle width
             .attr("height", 500)     // rectangle height
             .attr("preserveAspectRation", "none");
          cards[count] = card;
          console.log("count: ", count);
+         column++;
          count++;
+         if (count % 5 == 0) {
+            row++;
+            column = 0;
+         }
       })
 
       return data;
@@ -61,7 +69,8 @@ function resize() {
         .attr("transform", "scale(" + relWidth + "," + relHeight + ")")
 }
 
-readCSV("../Data/Business Cards.csv");
+const data = readCSV("../Data/Business Cards.csv");
 resize();
 
 window.addEventListener("resize", resize);
+
